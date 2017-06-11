d82ff582d033a69e0f7a55d985a6560486f016b3
coolshou <coolshou2060@gmail.com>
2017-06-11 22:55:49 +0800

    rename qtdsync.qrc back to QtdSync.qrc

---
+++ qtdsync/uic_forms.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+uic forms/QtdSyncDlg.ui -o include/ui_QtdSyncDlg.h
+uic forms/QtdSyncStartupDlg.ui -o include/ui_QtdSyncStartupDlg.h
+uic forms/QtdSyncConfigDlg.ui -o include/ui_QtdSyncConfigDlg.h
+uic forms/QtdSyncAboutDlg.ui -o include/ui_QtdSyncAboutDlg.h
+uic forms/QtdSyncAuthDlg.ui -o include/ui_QtdSyncAuthDlg.h
+uic forms/QtdSyncRemoteUserDlg.ui -o include/ui_QtdSyncRemoteUserDlg.h
+uic forms/QtdSyncUpdateDlg.ui -o include/ui_QtdSyncUpdateDlg.h
+uic forms/QtdSyncScheduledBackupDlg.ui -o include/ui_QtdSyncScheduledBackupDlg.h
+uic forms/QtdSyncSettingsDlg.ui -o include/ui_QtdSyncSettingsDlg.h
+uic forms/QtdSyncServerDlg.ui -o include/ui_QtdSyncServerDlg.h
+uic forms/QtdSyncVDirConfigDlg.ui -o include/ui_QtdSyncVDirConfigDlg.h
+uic forms/QtdSyncSSHFolderDlg.ui -o include/ui_QtdSyncSSHFolderDlg.h
+uic forms/QtdSyncFolderBindingDlg.ui -o include/ui_QtdSyncFolderBindingDlg.h
+uic forms/QtdSyncMailDlg.ui -o include/ui_QtdSyncMailDlg.h
+uic forms/QtdSyncScheduleDlg.ui -o include/ui_QtdSyncScheduleDlg.h
